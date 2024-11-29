fb3d475e1ba147d4409bb43e756c6d9145d5e7b0
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2024-11-29 19:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (8.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,33 +2,49 @@ class Solution {
 public:
     int trap(vector<int>& height) {
       int n=height.size();  
-      vector<int>rmax(n);
-      rmax[n-1]=0;
-vector<int>lmax(n);
-lmax[0]=0;
-int left=height[0];
-for(int i=1;i<n;i++){
-left=max(left,height[i]);
-lmax[i]=left;
-
-}
-int right=height[n-1];
-for(int i=n-2;i>=0;i--){
-right=max(right,height[i]);
-rmax[i]=right;
-}
-
-int water=0;
-int currheight=0;
-int waterhold=0;
+//   approach is simple find the tallest building now from start building to that tallest builoding we will first find 
+     // water stored considering the tallest rightmx and similarly from that tallest building to the rightmost building
+      int tallest=0;
+      int index=0;
 for(int i=0;i<n;i++){
-    if(height[i]<lmax[i] && height[i]<rmax[i]){
-currheight=min(lmax[i],rmax[i]);
-waterhold=currheight-height[i];
-water=water+waterhold;
+        //  tallest=max(tallest,height[i]);
+    if(tallest<height[i]){
+        tallest=height[i];
+        index=i;
     }
 }
-      return water;
+int left=height[0];
+int water=0;
+// see neighter the first building will store the water nor the last buiolding
+for(int i=1;i<index;i++){
+
+if(height[i]<left){
+  water+=left-height[i];// why is this building storing water because curr buil ht is lesser than the left one 
+//   and it is already shorter than the tallest one which is on the right side of it 
+}
+else{
+    left=height[i];
+    // doing this because if current building height is greater the left building which was 
+        // tallest  tilll now then water storage would be 0 but the left would upgrade to the curr building height
+   
+}
+}
+//  for index where the tallest building currently is , there no water would be logged , therefore not considering that building for
+// the water storage
+int right=height[n-1];
+
+// starting from the second last building as last wont be storing any
+for(int i=n-2;i>index;i--){
+    if(height[i]<right){
+        water+=right-height[i];
+    }
+    else{
+        right=height[i];// doing this because if current building height is greater the right building which was 
+        // tallest  tilll now then water storage would be 0 but the right would upgrade to the curr building height
+    }
+}
+return water;
+
 
     }
 };
