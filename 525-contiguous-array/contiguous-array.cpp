afcae4530d41c288080a87a214af4b36bb04ee64
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2024-12-21 10:17:15 +0530

    Time: 19 ms (89.78%) | Memory: 88.3 MB (61.35%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int,int>m; //key =diff & val=ind
+        unordered_map<int,int>m; 
         int n=nums.size();
         int cnt=0;
         int maxx=0;
@@ -17,14 +17,14 @@ public:
                 cnt-=1;
             }
 
-            //cehck if diff is already 
+            
             if(m.find(cnt)!=m.end())
             {
                 maxx=max(maxx,i-m[cnt]);
             }
             else
             {
-                //since val is index
+                
                 m[cnt]=i;
             }
         }
