6bc49e88dfb7cbed3fa118a80eda617832103bb6
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-01-26 09:53:36 +0530

    Time: 2 ms (75.78%) | Memory: 53.3 MB (38.15%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        // this is not optimized as i m learning java and doing cod ein this
-        HashMap<Integer, Integer>mpp=new HashMap<>();
-    
-// for(auto it:nums){
-//     mpp[it]++;
-// }
-    // mpp.put(it)
-for (int it : nums) {
-            mpp.put(it, mpp.getOrDefault(it, 0) + 1);
-        }
-        for(int i=0;i<nums.length;i++){
-            if(mpp.get(nums[i])>nums.length/2){
-              return  nums[i];
-            }
-        }
-        return 1;
+    public int majorityElement(int[] num) {
+        
+        int count=0;
+        int hero=-1;
+        for(int i=0;i<num.length;i++)
+        {
+            if(count==0)hero=num[i];
 
+if(num[i]==hero)count++;
+else{
+    count--;
+}
+
+        }
+return hero;
     }
 }
