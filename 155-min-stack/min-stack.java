c128428092de7189507679d9d9084b9a7a4b8f8e
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-01-29 18:23:37 +0530

    Time: 11 ms (5.02%) | Memory: 44.8 MB (68.58%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,35 +1,47 @@
-import java.util.Stack;
 
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> main;
-
+Stack<Integer>st;
+Stack <Integer>main;
     public MinStack() {
-        st = new Stack<>();
-        main = new Stack<>();
+        st=new Stack<>();
+        main=new Stack<>();
     }
-
+    
     public void push(int val) {
         main.push(val);
-        if (st.isEmpty() || val <= st.peek()) {
+        if(st.size()==0 || val<=st.peek()){
             st.push(val);
         }
     }
-
+    
     public void pop() {
-        if (!main.isEmpty()) {
-            if (!st.isEmpty() && st.peek().equals(main.peek())) {
-                st.pop();
-            }
-            main.pop();
-        }
-    }
+        // if(!st.isEmpty())
+        // if(st.peek()==main.peek()){
+        //     st.pop();
+        // }
 
+int a=st.peek();
+int b=main.pop();
+if(a==b)st.pop();
+    //    if(!main.isEmpty()) main.pop();
+    }
+    
     public int top() {
-        return main.isEmpty() ? -1 : main.peek(); // Edge case: If empty, return -1
+        return main.peek();
     }
-
+    
     public int getMin() {
-        return st.isEmpty() ? -1 : st.peek(); // Edge case: If empty, return -1
+
+        // if(st.size()>0)return st.peek();
+        return st.size()!=0?st.peek():-1;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
