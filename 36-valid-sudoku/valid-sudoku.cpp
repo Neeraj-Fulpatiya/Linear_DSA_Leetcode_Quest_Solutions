1ad206c4d039ba6670d891a36ac41931387e9db3
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-02-24 18:42:44 +0530

    Time: 15 ms (7.35%) | Memory: 25.3 MB (32.17%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         set<string>s;
-        for(int i=0;i<9;i++){
+        // see we made set so that no duplicates are there ,, you will undrstnd that in code below
+        // one more thing to note that we will store strings in set as say "row19 "(means in row 1 ,9 has come), if this comes again means that the same row has encountered the 9 again
+
+// note-- and this format we can store only using the string 
+
+        for(int i=0;i<9;i++){// this is whaat it is
             for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                    string row="row"+to_string(i)+board[i][j];
+                if(board[i][j]!='.'){// we does not care for the .(dots), as they have no significance
+                    
+                    string row="row"+to_string(i)+board[i][j];// making the row in string format, same with col and box
                     string col="col"+to_string(j)+board[i][j];
-                    string box="box"+to_string((i/3)*3+j/3)+board[i][j];
+                    string box="box"+to_string((i/3)*3+j/3)+board[i][j];// this formula u can have derivation by your way 
                    
-                   if(s.find(row)==s.end() && s.find(col)==s.end()&& s.find(box)==s.end()){
+                   if(s.find(row)==s.end() && s.find(col)==s.end()&& s.find(box)==s.end()){//this means i try to find 3 of these and unable to find , so now we cna insert
                     s.insert(row);
 
                     s.insert(col);
                     s.insert(box);
                    }
-                   else{
+
+                   else{// if any of the row , col , box has the same val then this will execute
                     return false;
                    }
 
@@ -23,6 +30,6 @@ public:
 
             }
         }
-        return true;
+        return true;// if nothing has gone to flase then therefore suduko is valid 
     }
 };
