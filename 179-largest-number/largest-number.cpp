423aede41ae371a5c95a48b77aad0bb6fdc9b0b7
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-03-02 13:07:29 +0530

    Time: 3 ms (78.29%) | Memory: 17.3 MB (6.53%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-bool comp(string a,string b){
-return a+b>b+a;
-}
-    string largestNumber(vector<int>& nums) {
-sort(nums.rbegin(),nums.rend());
-
-
-
-vector<string>str;
-// for(int )
-for(auto p:nums){
-    str.push_back(to_string(p));
-
-}
-
-sort(str.begin(), str.end(), [](const string &a, const string &b) {
-    return a + b > b + a;  // Custom sorting logic
-});  
-
-string ans="";
-for(auto s:str){
-    ans+=s;
-}
-if(ans[0]=='0')return "0";
-return ans;
-
-
-
+    class Solution {
+    public:
+    bool comp(string a,string b){
+    return a+b>b+a;
     }
-};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        string largestNumber(vector<int>& nums) {
+    sort(nums.rbegin(),nums.rend());
+
+
+
+    vector<string>str;
+    // for(int )
+    for(auto p:nums){
+        str.push_back(to_string(p));
+    
+    }
+
+    sort(str.begin(), str.end(), [](const string &a, const string &b) {
+        return a + b > b + a;  // Custom sorting logic
+    });  
+
+    string ans="";
+    for(auto s:str){
+        ans+=s;
+    }
+    if(ans[0]=='0')return "0";
+    return ans;
+
+
+
+        }
+    };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
