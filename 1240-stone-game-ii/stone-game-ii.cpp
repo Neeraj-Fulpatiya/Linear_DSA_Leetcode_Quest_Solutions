df2fc38a2facab60fdae6d22716f1f1df8094c11
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-06-29 09:53:13 +0530

    Time: 16 ms (47.14%) | Memory: 11.7 MB (79.70%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,51 @@
+
+class Solution {
+private:
+    int n;
+    int dp[3][101][101];
+    vector<int> pref;
+    int solveForAlice(int i, int player, int M, vector<int>& piles) {
+        if(i >= n) return 0;
+        if(dp[player][i][M] != -1) return dp[player][i][M];
+        int result;
+        if(player == 1){ 
+            result = -1;  
+        }else{  
+            result = INT_MAX;   
+        }
+         for (int X = 1; X <= min(2 * M, n - i); X++){
+            int currentStones;
+            if(i + X <= n){
+                if(i > 0){
+                    currentStones = pref[i + X - 1] - pref[i - 1];
+                }else{
+                    currentStones = pref[i + X - 1];
+                }
+            }else{
+                currentStones = 0;
+            }
+            if(player == 1){
+                
+                result = max(result, currentStones + solveForAlice(i + X, 0, max(M, X), piles));
+            }else{
+                 
+                result = min(result, solveForAlice(i + X, 1, max(M, X), piles));
+            }
+        }
+
+        dp[player][i][M] = result;
+        return result;
+    }
+
+public:
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+        memset(dp, -1, sizeof(dp));
+        pref.resize(n, 0);
+        pref[0] = piles[0]; 
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + piles[i];
+        }
+        return solveForAlice(0, 1, 1, piles);
+    }
+};
