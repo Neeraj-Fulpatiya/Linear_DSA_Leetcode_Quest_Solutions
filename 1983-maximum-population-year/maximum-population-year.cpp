3eb063bac6ef54d28d4b76e76e9edcd589cfe797
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-07-02 15:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (91.41%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-    int pop[2051] = {}, res = 0;
-    for (auto &l : logs) {
-        ++pop[l[0]];
-        --pop[l[1]];
+        int years[101] = {0};
+        for(auto &log : logs) {
+            years[log[0] - 1950]++;
+            years[log[1] - 1950]--;
+        }
+        int totalAlive = 0;
+        int maxAlive = 0;
+        for(auto &it : years) {
+            totalAlive += it;
+            it = totalAlive;
+            maxAlive = max(maxAlive, totalAlive);
+        }
+        int y = 1950;
+        for(; y <= 2050; ++y) {
+            if(years[y - 1950] == maxAlive)
+                break;
+        }
+        return y;
     }
-    for (auto i = 1950; i < 2050; ++i) {
-        pop[i] += pop[i - 1];
-        res = pop[i] > pop[res] ? i : res;
-    }
-    return res;
-}
 };
