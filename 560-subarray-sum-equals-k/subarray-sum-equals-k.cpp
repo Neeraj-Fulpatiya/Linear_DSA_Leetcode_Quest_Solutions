2600d7b8fd4eced66fe8c81659ea8b2234d8d3af
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2026-01-27 12:06:38 +0530

    Time: 2494 ms (10.69%) | Memory: 34.4 MB (81.00%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ans = 0;
-        int n = nums.size();
-        unordered_map<int,int>m;
-m[0]=1;
-int psum=0;
-        for(int i=0;i<n;i++){
-psum+=nums[i];
-if(m.find(psum-k)!=m.end()){
-    ans=ans+m[psum-k];
-    m[psum]++;
-}
-else{
-m[psum]++;
+        // vector<int>pSum;
+int ans=0;
+int n=nums.size();
+for(int i=0;i<n;i++){
+    int sum=0;
+    for(int j=i;j<n;j++){
+        sum+=nums[j];
+        if(sum==k)ans++;
+    }
 }
 
-        }
-        
-        return ans;
+//         unordered_map<int,int>m;
+// m[0]=1;
+//         int sum=0;
+//         for(auto s:nums){
+//             sum+=s;
+//             // pSum.push_back(sum);
+//             m[sum]++;
+//         }
+//         int ans=0;
+// for(auto i=m.begin();i!=m.end();i++){
+//     int seek=k-i->first;
+//     if(m.count(seek)){
+// ans=ans+i->second;
+//     }
+// }
+return ans;
     }
 };
