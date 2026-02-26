47b7e5a68bb3ebd05c87c8386e67aeb878630b1c
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2026-02-26 16:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.91%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        auto idxFinder = [&](int temp)->pair<int, int>{
+            int i = temp/m;
+            int j = temp%m;
+            return {i, j};
+        };
+        int l = 0, r = n*m - 1;
+        while(l <= r){
+            int mid = (l + r)/2;
+            auto [i, j] = idxFinder(mid);
+            if(matrix[i][j] == target) return true;
+            if(matrix[i][j] > target){
+                r = mid - 1;
+            }
+            else l = mid + 1;
+        }
+        return false;
+    }
+};
