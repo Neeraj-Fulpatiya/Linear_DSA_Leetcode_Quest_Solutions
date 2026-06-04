3ed2bce819999b0af99ff3426251d710d20884e8
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2026-06-05 01:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.21%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int ptr1 = m - 1;
-        int ptr2 = n - 1;
-        int end = m + n - 1;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) { 
 
-        while (ptr1 >= 0 && ptr2 >= 0) {
-            if (nums1[ptr1] <= nums2[ptr2]) {
-                nums1[end--] = nums2[ptr2--];
-            } else {
-                nums1[end--] = nums1[ptr1--];
+        int left=m-1;
+        int right=n-1;
+
+        int idx=m+n-1;
+
+
+        while(left>=0 && right>=0)
+        {
+            if(nums1[left]>nums2[right]) 
+            {
+                nums1[idx]=nums1[left];
+                left=left-1;
             }
+            else
+            {
+                nums1[idx]=nums2[right];
+                right=right-1;
+            }
+
+            idx=idx-1;
         }
 
-        while (ptr2 >= 0) {
-            nums1[end--] = nums2[ptr2--];
+        while(right>=0)
+        {
+            nums1[idx]=nums2[right];
+            idx=idx-1;
+            right=right-1;
         }
-    }
+
+        }
 };
