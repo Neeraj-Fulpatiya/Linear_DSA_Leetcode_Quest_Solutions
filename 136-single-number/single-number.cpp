55ee37a5777d120e4de862db336a5ef6bbdd4694
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2026-07-06 11:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (24.09%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a = 0;
+        for(auto it : nums){
+            a = a ^ it;
+        }
+        return a;
+    }
+};
